4916bc3117cb04d30b48a69af870aa8ef97fdc0b
danielmk <danielkup3@gmail.com>
2023-11-11 16:51:11 +0200

    laing the groundwork part 2

--- map_space_manager.cpp
+++ map_space_manager.cpp
@@ -2,4 +2,194 @@
 // Created by danie on 11/11/2023.
 //
 
+#include <stdlib.h>
+
 #include "map_space_manager.h"
+
+typedef struct Node_t {}* Node;
+
+Node createnode();
+void destroynode();
+Node copynode();
+
+struct map_t{
+    Node head;
+    copyDataElements copyDataFunc;
+    copyKeyElements copykeyfunc;
+    freeDataElements freedata;
+    freeKeyElements fereekey;
+    compareKeyElements comparisonfunc;
+};
+
+/**
+* mapCreate: Allocates a new empty map.
+*
+* @param copyDataElement - Function pointer to be used for copying data elements into
+*  	the map or when copying the map.
+* @param copyKeyElement - Function pointer to be used for copying key elements into
+*  	the map or when copying the map.
+* @param freeDataElement - Function pointer to be used for removing data elements from
+* 		the map
+* @param freeKeyElement - Function pointer to be used for removing key elements from
+* 		the map
+* @param compareKeyElements - Function pointer to be used for comparing key elements
+* 		inside the map. Used to check if new elements already exist in the map.
+* @return
+* 	NULL - if one of the parameters is NULL or allocations failed.
+* 	A new Map in case of success.
+*/
+Map mapCreate(copyDataElements copyDataElement,
+              copyKeyElements copyKeyElement,
+              freeDataElements freeDataElement,
+              freeKeyElements freeKeyElement,
+              compareKeyElements compareKeyElements)
+{
+    if(!copyDataElement||!copyKeyElement||!freeDataElement||
+       !freeKeyElement||!compareKeyElements)
+    {
+        return NULL;
+    }
+    Map mapnew = static_cast<Map>(malloc(sizeof(struct map_t)));
+    if(!mapnew)
+    {
+        return NULL;
+    }
+    mapnew->comparisonfunc=compareKeyElements;
+    mapnew->copyDataFunc=copyDataElement;
+    mapnew->copykeyfunc=copyKeyElement;
+    mapnew->fereekey=freeKeyElement;
+    mapnew->freedata=freeDataElement;
+    mapnew->head=NULL;
+}
+
+/**
+* mapDestroy: Deallocates an existing map. Clears all elements by using the
+* stored free functions.
+*
+* @param map - Target map to be deallocated. If map is NULL nothing will be
+* 		done
+*/
+void mapDestroy(Map map);
+
+/**
+* mapCopy: Creates a copy of target map.
+* Iterator values for both maps is undefined after this operation.
+*
+* @param map - Target map.
+* @return
+* 	NULL if a NULL was sent or a memory allocation failed.
+* 	A Map containing the same elements as map otherwise.
+*/
+Map mapCopy(Map map);
+
+/**
+* mapGetSize: Returns the number of elements in a map
+* @param map - The map which size is requested
+* @return
+* 	-1 if a NULL pointer was sent.
+* 	Otherwise the number of elements in the map.
+*/
+int mapGetSize(Map map);
+
+/**
+* mapContains: Checks if a key element exists in the map. The key element will be
+* considered in the map if one of the key elements in the map it determined equal
+* using the comparison function used to initialize the map.
+*
+* @param map - The map to search in
+* @param element - The element to look for. Will be compared using the
+* 		comparison function.
+* @return
+* 	false - if one or more of the inputs is null, or if the key element was not found.
+* 	true - if the key element was found in the map.
+*/
+bool mapContains(Map map, KeyElement element);
+
+/**
+*	mapPut: Gives a specified key a specific value.
+*  Iterator's value is undefined after this operation.
+*
+* @param map - The map for which to reassign the data element
+* @param keyElement - The key element which need to be reassigned
+* @param dataElement - The new data element to associate with the given key.
+*      A copy of the element will be inserted as supplied by the copying function
+*      which is given at initialization and old data memory would be
+*      deleted using the free function given at initialization.
+* @return
+* 	MAP_NULL_ARGUMENT if a NULL was sent as map
+* 	MAP_OUT_OF_MEMORY if an allocation failed (Meaning the function for copying
+* 	an element failed)
+* 	MAP_SUCCESS the paired elements had been inserted successfully
+*/
+MapResult mapPut(Map map, KeyElement keyElement, DataElement dataElement);
+
+/**
+*	mapGet: Returns the data associated with a specific key in the map.
+*			Iterator status unchanged
+*
+* @param map - The map for which to get the data element from.
+* @param keyElement - The key element which need to be found and whos data
+we want to get.
+* @return
+*  NULL if a NULL pointer was sent or if the map does not contain the requested key.
+* 	The data element associated with the key otherwise.
+*/
+DataElement mapGet(Map map, KeyElement keyElement);
+
+/**
+* 	mapRemove: Removes a pair of key and data elements from the map. The elements
+*  are found using the comparison function given at initialization. Once found,
+*  the elements are removed and deallocated using the free functions
+*  supplied at initialization.
+*  Iterator's value is undefined after this operation.
+*
+* @param map -
+* 	The map to remove the elements from.
+* @param keyElement
+* 	The key element to find and remove from the map. The element will be freed using the
+* 	free function given at initialization. The data element associated with this key
+*  will also be freed using the free function given at initialization.
+* @return
+* 	MAP_NULL_ARGUMENT if a NULL was sent to the function
+*  MAP_ITEM_DOES_NOT_EXIST if an equal key item does not already exists in the map
+* 	MAP_SUCCESS the paired elements had been removed successfully
+*/
+MapResult mapRemove(Map map, KeyElement keyElement);
+
+/**
+*	mapGetFirst: Sets the internal iterator (also called current key element) to
+*	the first key element in the map. There doesn't need to be an internal order
+*  of the keys so the "first" key element is any key element.
+*	Use this to start iterating over the map.
+*	To continue iteration use mapGetNext
+*
+* @param map - The map for which to set the iterator and return the first
+* 		key element.
+* @return
+* 	NULL if a NULL pointer was sent or the map is empty.
+* 	The first key element of the map otherwise
+*/
+KeyElement mapGetFirst(Map map);
+
+/**
+*	mapGetNext: Advances the map iterator to the next key element and returns it.
+*	The next key element is any key element not previously returned by the iterator.
+* @param map - The map for which to advance the iterator
+* @return
+* 	NULL if reached the end of the map, or the iterator is at an invalid state
+* 	or a NULL sent as argument
+* 	The next key element on the map in case of success
+*/
+KeyElement mapGetNext(Map map);
+
+
+/**
+* mapClear: Removes all key and data elements from target map.
+* The elements are deallocated using the stored free functions.
+* @param map
+* 	Target map to remove all element from.
+* @return
+* 	MAP_NULL_ARGUMENT - if a NULL pointer was sent.
+* 	MAP_SUCCESS - Otherwise.
+*/
+MapResult mapClear(Map map);
--- map_space_manager.h
+++ map_space_manager.h
@@ -5,91 +5,242 @@
 #ifndef QUEENS_GAMBIT_THE_FIX_MAP_SPACE_MANAGER_H
 #define QUEENS_GAMBIT_THE_FIX_MAP_SPACE_MANAGER_H
 
+#include <stdbool.h>
+
 /**
- * map  adt
- *
- * Implements a sorted list by a user
- *
- *
- * The following functions are available:
- *   mapCreate            - Creates a new and empty map
- *   mapDestroy           - Deletes an existing and frees all resources
- *   mapCopy              - Copies an existing map
- *   mapGetSize           – returns the size of the dictionary
- *   mapContains          – serches for an element in the dictionary
- *   mapPut               – changes a value in the dictionary or creating new if the key doesn't exist
- *   mapGet               - given a key, returns a value
- *   mapRemove            – remove a given key
- *   mapGetFirst          – restore the iterator to the first entry in the dictionary and returnes the value of the
- *                          first key
- *   mapGetNext           – ascending the iterator and returning a copy of the value
- *   mapClear             – remove all data from the dictionary
- */
+* Generic Map Container
+*
+* Implements a map container type.
+* The map has an internal iterator for external use. For all functions
+* where the state of the iterator after calling that function is not stated,
+* it is undefined. That is you cannot assume anything about it.
+*
+* The following functions are available:
+*   mapCreate		- Creates a new empty map
+*   mapDestroy		- Deletes an existing map and frees all resources
+*   mapCopy		- Copies an existing map
+*   mapGetSize		- Returns the size of a given map
+*   mapContains	- returns weather or not a key exists inside the map.
+*   				  This resets the internal iterator.
+*   mapPut		    - Gives a specific key a given value.
+*   				  If the key exists, the value is overridden.
+*   				  This resets the internal iterator.
+*   mapGet  	    - Returns the data paired to a key which matches the given key.
+*					  Iterator status unchanged
+*   mapRemove		- Removes a pair of (key,data) elements for which the key
+*                    matches a given element (by the key compare function).
+*   				  This resets the internal iterator.
+*   mapGetFirst	- Sets the internal iterator to the first key in the
+*   				  map, and returns it.
+*   mapGetNext		- Advances the internal iterator to the next key and
+*   				  returns it.
+*	 mapClear		- Clears the contents of the map. Frees all the elements of
+*	 				  the map using the free function.
+* 	 MAP_FOREACH	- A macro for iterating over the map's elements.
+*/
+
+/** Type for defining the map */
+typedef struct map_t* Map;
+
+/** Type used for returning error codes from map functions */
+typedef enum MapResult_t {
+    MAP_SUCCESS,
+    MAP_OUT_OF_MEMORY,
+    MAP_NULL_ARGUMENT,
+    MAP_ITEM_ALREADY_EXISTS,
+    MAP_ITEM_DOES_NOT_EXIST
+} MapResult;
+
+/** Data element data type for map container */
+typedef void *DataElement;
+
+/** Key element data type for map container */
+typedef void *KeyElement;
+
+/** Type of function for copying a data element of the map */
+typedef DataElement(*copyDataElements)(DataElement);
+
+/** Type of function for copying a key element of the map */
+typedef KeyElement(*copyKeyElements)(KeyElement);
+
+/** Type of function for deallocating a data element of the map */
+typedef void(*freeDataElements)(DataElement);
+
+/** Type of function for deallocating a key element of the map */
+typedef void(*freeKeyElements)(KeyElement);
 
 
-typedef void* Key;
+/**
+* Type of function used by the map to identify equal key elements.
+* This function should return:
+* 		A positive integer if the first element is greater;
+* 		0 if they're equal;
+*		A negative integer if the second element is greater.
+*/
+typedef int(*compareKeyElements)(KeyElement, KeyElement);
 
-typedef void* Data;
+/**
+* mapCreate: Allocates a new empty map.
+*
+* @param copyDataElement - Function pointer to be used for copying data elements into
+*  	the map or when copying the map.
+* @param copyKeyElement - Function pointer to be used for copying key elements into
+*  	the map or when copying the map.
+* @param freeDataElement - Function pointer to be used for removing data elements from
+* 		the map
+* @param freeKeyElement - Function pointer to be used for removing key elements from
+* 		the map
+* @param compareKeyElements - Function pointer to be used for comparing key elements
+* 		inside the map. Used to check if new elements already exist in the map.
+* @return
+* 	NULL - if one of the parameters is NULL or allocations failed.
+* 	A new Map in case of success.
+*/
+Map mapCreate(copyDataElements copyDataElement,
+              copyKeyElements copyKeyElement,
+              freeDataElements freeDataElement,
+              freeKeyElements freeKeyElement,
+              compareKeyElements compareKeyElements);
 
-typedef void(*KeyCompare)(Key,Key);
-
-typedef struct Map_t* Map;
-/*
- *   mapCreate            - Creates a new and empty map
- *   @param- KeyCompare
- *   @return
- *    empty map
- *    NULL otherwise
- */
-Map mapCreate(KeyCompare compare);
-
-/*
- *   mapDestroy           - Deletes an existing and frees all resources
- *   @param- map
- */
+/**
+* mapDestroy: Deallocates an existing map. Clears all elements by using the
+* stored free functions.
+*
+* @param map - Target map to be deallocated. If map is NULL nothing will be
+* 		done
+*/
 void mapDestroy(Map map);
 
-/*
- *   mapCopy              - Copies an existing map
- *   @param- map
- *   @return-
- *            map if the copy process was sucsessful
- *            NULL otherwise
- */
+/**
+* mapCopy: Creates a copy of target map.
+* Iterator values for both maps is undefined after this operation.
+*
+* @param map - Target map.
+* @return
+* 	NULL if a NULL was sent or a memory allocation failed.
+* 	A Map containing the same elements as map otherwise.
+*/
 Map mapCopy(Map map);
 
-/*
- *   mapGetSize           – returns the size of the dictionary
- *   @param-  map
- *   @return-
- *            -1 if the param was NULL
- *            positive number or 0 otherwise
- */
+/**
+* mapGetSize: Returns the number of elements in a map
+* @param map - The map which size is requested
+* @return
+* 	-1 if a NULL pointer was sent.
+* 	Otherwise the number of elements in the map.
+*/
 int mapGetSize(Map map);
 
-/*
- *   mapContains          – serches for an element in the dictionary
- *   @param- Key  -the element we are serching
- *           Map  -the place where we want to serch
- *   @return-TRUE if the key is in the map
- *           FALSE otherwise
- */
-bool mapContains(Map map, Key key);
+/**
+* mapContains: Checks if a key element exists in the map. The key element will be
+* considered in the map if one of the key elements in the map it determined equal
+* using the comparison function used to initialize the map.
+*
+* @param map - The map to search in
+* @param element - The element to look for. Will be compared using the
+* 		comparison function.
+* @return
+* 	false - if one or more of the inputs is null, or if the key element was not found.
+* 	true - if the key element was found in the map.
+*/
+bool mapContains(Map map, KeyElement element);
 
-/*
- *   mapPut               – changes a value in the dictionary or creating new if the key doesn't exist
- *   @param-    Map
- *              Key  -the key we want to change or add
- *              Data -the data we want to enter
- *   @return-   MAP_SUCSESS        -the operation was a sucsess
- *              MAP_NULL_ARGUMENT  -a NULL argument was passed
- *              MAP_ERROR          -otherwise
- *   mapGet               - given a key, returns a value
- *   mapRemove            – remove a given key
- *   mapGetFirst          – restore the iterator to the first entry in the dictionary and returnes the value of the
- *                          first key
- *   mapGetNext           – ascending the iterator and returning a copy of the value
- *   mapClear             – remove all data from the dictionary
- */
+/**
+*	mapPut: Gives a specified key a specific value.
+*  Iterator's value is undefined after this operation.
+*
+* @param map - The map for which to reassign the data element
+* @param keyElement - The key element which need to be reassigned
+* @param dataElement - The new data element to associate with the given key.
+*      A copy of the element will be inserted as supplied by the copying function
+*      which is given at initialization and old data memory would be
+*      deleted using the free function given at initialization.
+* @return
+* 	MAP_NULL_ARGUMENT if a NULL was sent as map
+* 	MAP_OUT_OF_MEMORY if an allocation failed (Meaning the function for copying
+* 	an element failed)
+* 	MAP_SUCCESS the paired elements had been inserted successfully
+*/
+MapResult mapPut(Map map, KeyElement keyElement, DataElement dataElement);
+
+/**
+*	mapGet: Returns the data associated with a specific key in the map.
+*			Iterator status unchanged
+*
+* @param map - The map for which to get the data element from.
+* @param keyElement - The key element which need to be found and whos data
+we want to get.
+* @return
+*  NULL if a NULL pointer was sent or if the map does not contain the requested key.
+* 	The data element associated with the key otherwise.
+*/
+DataElement mapGet(Map map, KeyElement keyElement);
+
+/**
+* 	mapRemove: Removes a pair of key and data elements from the map. The elements
+*  are found using the comparison function given at initialization. Once found,
+*  the elements are removed and deallocated using the free functions
+*  supplied at initialization.
+*  Iterator's value is undefined after this operation.
+*
+* @param map -
+* 	The map to remove the elements from.
+* @param keyElement
+* 	The key element to find and remove from the map. The element will be freed using the
+* 	free function given at initialization. The data element associated with this key
+*  will also be freed using the free function given at initialization.
+* @return
+* 	MAP_NULL_ARGUMENT if a NULL was sent to the function
+*  MAP_ITEM_DOES_NOT_EXIST if an equal key item does not already exists in the map
+* 	MAP_SUCCESS the paired elements had been removed successfully
+*/
+MapResult mapRemove(Map map, KeyElement keyElement);
+
+/**
+*	mapGetFirst: Sets the internal iterator (also called current key element) to
+*	the first key element in the map. There doesn't need to be an internal order
+*  of the keys so the "first" key element is any key element.
+*	Use this to start iterating over the map.
+*	To continue iteration use mapGetNext
+*
+* @param map - The map for which to set the iterator and return the first
+* 		key element.
+* @return
+* 	NULL if a NULL pointer was sent or the map is empty.
+* 	The first key element of the map otherwise
+*/
+KeyElement mapGetFirst(Map map);
+
+/**
+*	mapGetNext: Advances the map iterator to the next key element and returns it.
+*	The next key element is any key element not previously returned by the iterator.
+* @param map - The map for which to advance the iterator
+* @return
+* 	NULL if reached the end of the map, or the iterator is at an invalid state
+* 	or a NULL sent as argument
+* 	The next key element on the map in case of success
+*/
+KeyElement mapGetNext(Map map);
+
+
+/**
+* mapClear: Removes all key and data elements from target map.
+* The elements are deallocated using the stored free functions.
+* @param map
+* 	Target map to remove all element from.
+* @return
+* 	MAP_NULL_ARGUMENT - if a NULL pointer was sent.
+* 	MAP_SUCCESS - Otherwise.
+*/
+MapResult mapClear(Map map);
+
+/*!
+* Macro for iterating over a map.
+* Declares a new iterator for the loop.
+*/
+#define MAP_FOREACH(type, iterator, map) \
+    for(type iterator = (type) mapGetFirst(map) ; \
+        iterator ;\
+        iterator = mapGetNext(map))
 
 #endif //QUEENS_GAMBIT_THE_FIX_MAP_SPACE_MANAGER_H
